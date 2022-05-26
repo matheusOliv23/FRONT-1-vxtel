55ea90848ad119ed50ed1e167e57a73b57d835ba
matheusoliv23 <matheushenri1996@gmail.com>
2022-05-26 07:30:04 -0300

    feat Hero Component

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,20 @@
+import { Button } from 'components/Button'
+import styles from './styles.module.scss'
+
+export const Hero = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.heroContainer}>
+        <h1 className={styles.title}>Fale mais com quem você ama!</h1>
+        <p className={styles.paragraph}>
+          Com os planos <strong>FaleMais</strong> você fala e a gente paga.
+          Tenha minutos todos os dias para falar com quem quiser por nossa
+          conta, você só paga os minutos excedentes do seu plano.
+        </p>
+        <Button variant="primary" size="fullWidth">
+          Saiba mais
+        </Button>
+      </div>
+    </div>
+  )
+}
