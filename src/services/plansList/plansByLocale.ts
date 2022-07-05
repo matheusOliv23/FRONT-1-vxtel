c08ec3ceebae4d0512b0662c99ea00face7344e8
andresiqueira <andresiqueira-ti@outlook.com>
2022-07-05 12:40:53 -0300

    feat: create function calculatePrice and your dependence, planList

---
+++ src/services/plansList/plansByLocale.ts
@@ -0,0 +1,49 @@
+export const plansByLocale = (
+  qtdMinutes: number,
+  typePlan: number,
+  dddOrigin: string,
+  dddDest: string,
+) => {
+  let valueWithPlan = 0
+  let valueWithoutPlan = 0
+  const valuePerMinute = [1.9, 2.9, 1.7, 2.7, 0.9, 1.9]
+
+  //abstrair essa função
+  const calculatePlanValue = (
+    qtdMinutes: number,
+    typePlan: number,
+    valuePerMinute: number,
+  ) => {
+    if (qtdMinutes <= typePlan) {
+      valueWithPlan = 0
+    }
+    if (qtdMinutes > typePlan) {
+      const excess = qtdMinutes - typePlan
+      valueWithPlan = excess * (valuePerMinute + (1 / 10) * valuePerMinute)
+    }
+    valueWithoutPlan = qtdMinutes * valuePerMinute
+
+    return { valueWithPlan, valueWithoutPlan }
+  }
+
+  if (dddOrigin === '011' && dddDest === '016') {
+    calculatePlanValue(qtdMinutes, typePlan, valuePerMinute[0])
+  }
+  if (dddOrigin === '016' && dddDest === '011') {
+    calculatePlanValue(qtdMinutes, typePlan, valuePerMinute[1])
+  }
+  if (dddOrigin === '011' && dddDest === '017') {
+    calculatePlanValue(qtdMinutes, typePlan, valuePerMinute[2])
+  }
+  if (dddOrigin === '017' && dddDest === '011') {
+    calculatePlanValue(qtdMinutes, typePlan, valuePerMinute[3])
+  }
+  if (dddOrigin === '011' && dddDest === '018') {
+    calculatePlanValue(qtdMinutes, typePlan, valuePerMinute[4])
+  }
+  if (dddOrigin === '018' && dddDest === '011') {
+    calculatePlanValue(qtdMinutes, typePlan, valuePerMinute[5])
+  }
+
+  return { valueWithPlan, valueWithoutPlan }
+}
